6d1154e5e1ede39da98acdb453a538805ffbfd88
Denis Diaconu <denisdiaconu117@yahoo.com>
2021-02-22 17:03:20 +0100

    Added the random class for the quotes

--- lib/anime.rb
+++ lib/anime.rb
@@ -1,24 +1,32 @@
 require 'httparty'
 require 'nokogiri'
-url = 'https://www.reddit.com/r/quotes/.rss'
-page = HTTParty.get(url, {
-    headers: { 'User-Agent' => 'HTTParty'}
-})
-parse_page = Nokogiri::XML(page.body)
-xml_content = parse_page.to_s.split('<entry>')
-array = Array.new
-for x in xml_content
-    if not x.include?('author')
+
+class NewRandom
+  def urls
+    url = 'https://www.reddit.com/r/quotes/.rss'
+    page = HTTParty.get(url, {
+                          headers: { 'User-Agent' => 'HTTParty' }
+                        })
+    Nokogiri::XML(page.body)
+  end
+
+  def quotes
+    xml_content = urls.to_s.split('<entry>')
+    array = []
+    xml_content.each do |x|
+      if !x.include?('author')
         next
-    else
+      else
         entry = x.to_s.split('>')
-        for y in entry
-            if y.include?('title')
-                filter = y.delete('</')
-                filter = filter.gsub('title','')
-                array.push(filter)
-            end
+        entry.each do |y|
+          next unless y.include?('title')
+
+          filter = y.delete('</')
+          filter = filter.gsub('title', '')
+          array.push(filter)
         end
+      end
     end
+    array
+  end
 end
-puts array.sample
--- lib/main.rb
+++ lib/main.rb
@@ -1,12 +1,11 @@
 require 'telegram/bot'
-require_relative 'anime.rb'
+require_relative 'anime'
 token = '1681412553:AAHlaXyUqAGqHfmdGPO2b3f8xhzGiZpzIjU'
-
 Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
     case message.text
     when '/help'
-      bot.api.send_message(chat_id: message.chat.id, text: 'All the existing commands: /help, /info, /start, /stop')
+      bot.api.send_message(chat_id: message.chat.id, text: 'All the existing commands: /help, /info, /test, /start, /stop')
     when '/info'
       bot.api.send_message(chat_id: message.chat.id, text: "My name is Denis Diaconu and i'm a full-stack-web-Developer!")
       bot.api.send_photo(chat_id: message.chat.id, photo: Faraday::UploadIO.new('special.jpg', 'image/jpeg'))
@@ -14,6 +13,12 @@ Telegram::Bot::Client.run(token) do |bot|
       bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}, you can use /help")
     when '/stop'
       bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
+    when '/test'
+      quote = NewRandom.new.quotes.sample
+      while quote.length <= 5
+        quote = NewRandom.new.quotes.sample
+      end
+      bot.api.send_message(chat_id: message.chat.id, text: "Random quote:  #{quote}")
     end
   end
 end
