adf5907a31781be3760024c940591ab156188a93
Denis Diaconu <denisdiaconu117@yahoo.com>
2021-02-22 15:57:01 +0100

    test for random quotes

--- lib/main.rb
+++ lib/main.rb
@@ -1,12 +1,12 @@
 require 'telegram/bot'
-require_relative 'anime'
+require_relative 'anime.rb'
 token = '1681412553:AAHlaXyUqAGqHfmdGPO2b3f8xhzGiZpzIjU'
 
 Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
     case message.text
     when '/help'
-        bot.api.send_message(chat_id: message.chat.id, text: 'All the existing commands: /help, /info, /start, /stop')
+      bot.api.send_message(chat_id: message.chat.id, text: 'All the existing commands: /help, /info, /start, /stop')
     when '/info'
       bot.api.send_message(chat_id: message.chat.id, text: "My name is Denis Diaconu and i'm a full-stack-web-Developer!")
       bot.api.send_photo(chat_id: message.chat.id, photo: Faraday::UploadIO.new('special.jpg', 'image/jpeg'))
@@ -14,8 +14,6 @@ Telegram::Bot::Client.run(token) do |bot|
       bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}, you can use /help")
     when '/stop'
       bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
-    when '/stop'
-      bot.api.send_message(chat_id: message.chat.id, text: "My favorite quote is")
     end
   end
 end
