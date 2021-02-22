9840466a6129f05bf6e1ac2cdc1cdf33c59ab127
Denis Diaconu <denisdiaconu117@yahoo.com>
2021-02-22 14:48:59 +0100

    Started the first part of the bot

---
+++ lib/main.rb
@@ -0,0 +1,21 @@
+require 'telegram/bot'
+require_relative 'anime'
+token = '1681412553:AAHlaXyUqAGqHfmdGPO2b3f8xhzGiZpzIjU'
+
+Telegram::Bot::Client.run(token) do |bot|
+  bot.listen do |message|
+    case message.text
+    when '/help'
+        bot.api.send_message(chat_id: message.chat.id, text: 'All the existing commands: /help, /info, /start, /stop')
+    when '/info'
+      bot.api.send_message(chat_id: message.chat.id, text: "My name is Denis Diaconu and i'm a full-stack-web-Developer!")
+      bot.api.send_photo(chat_id: message.chat.id, photo: Faraday::UploadIO.new('special.jpg', 'image/jpeg'))
+    when '/start'
+      bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}, you can use /help")
+    when '/stop'
+      bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
+    when '/stop'
+      bot.api.send_message(chat_id: message.chat.id, text: "My favorite quote is")
+    end
+  end
+end
