e5f4372613d4174b7be98f9aa497b3c52d841193
Denis Diaconu <denisdiaconu117@yahoo.com>
2021-02-23 14:28:45 +0100

    added one more option

--- bin/main.rb
+++ bin/main.rb
@@ -1,21 +1,21 @@
 require 'telegram/bot'
-require_relative 'lib/random'
+require_relative '../lib/random'
 token = '1681412553:AAHlaXyUqAGqHfmdGPO2b3f8xhzGiZpzIjU'
 Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
     case message.text
     when '/help'
-      bot.api.send_message(chat_id: message.chat.id, text: 'All the existing commands: /help, /info, /quote, /start, /stop')
+      bot.api.send_message(chat_id: message.chat.id,
+                           text: 'All the existing commands: /help, /info, /quote, /start, /stop')
     when '/info'
-      bot.api.send_message(chat_id: message.chat.id, text: "My name is Denis Diaconu and i'm a full-stack-web-Developer!")
+      bot.api.send_message(chat_id: message.chat.id,
+                           text: "My name is Denis Diaconu and i'm a full-stack-web-Developer! \nYou can find me on github at: https://github.com/denisdiaconu")
       bot.api.send_photo(chat_id: message.chat.id, photo: Faraday::UploadIO.new('special.jpg', 'image/jpeg'))
     when '/start'
       bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}, you can use /help")
     when '/quote'
       quote = NewRandom.new.quotes.sample
-      while quote.length <= 5
-        quote = NewRandom.new.quotes.sample
-      end
+      quote = NewRandom.new.quotes.sample while quote.length <= 5
       bot.api.send_message(chat_id: message.chat.id, text: "Random quote:  #{quote}")
     when '/stop'
       bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
